80c2fc2bbcfa448785459d483a3142c06944ec5f
oskvortsov <o.skvortsov.developer@gmail.com>
2022-05-17 11:49:52 +0300

    return auth

--- backend/Controllers/Employee/EmployeesController.cs
+++ backend/Controllers/Employee/EmployeesController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace backend.Controllers;
 
-// [Authorize]
+[Authorize]
 [ApiController]
 [Route("employee")]
 public class EmployeesController: ControllerBase
